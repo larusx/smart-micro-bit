eb7c0c14fd0f3839df1e485a329a142536f97ce3
larusx <larusx@163.com>
2023-12-03 09:12:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -54,6 +54,8 @@ function 停止 () {
     0
     )
 }
+let 右侧循迹传感器 = 0
+let 左侧循迹传感器 = 0
 basic.showIcon(IconNames.Happy)
 TM1650.showNumber(100)
 while (!(input.buttonIsPressed(Button.A))) {
@@ -61,13 +63,6 @@ while (!(input.buttonIsPressed(Button.A))) {
 }
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
     if (ps2controller.button_pressed(ps2controller.PS2Button.Up) == 1) {
         前进()
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.Down) == 1) {
@@ -79,18 +74,28 @@ basic.forever(function () {
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.Triangle) == 1) {
         makerobo.MotorRun(makerobo.Motors.Centre, 255)
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.Square) == 1) {
-        music.play(music.stringPlayable("E D C D E E E E ", 120), music.PlaybackMode.UntilDone)
-        music.play(music.stringPlayable("D D D D E E E E ", 120), music.PlaybackMode.UntilDone)
-        music.play(music.stringPlayable("E D C D E E E E ", 120), music.PlaybackMode.UntilDone)
-        music.play(music.stringPlayable("D D E D C C C C ", 120), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("E D C D E E E E ", 120), music.PlaybackMode.InBackground)
+        music.play(music.stringPlayable("D D D D E E E E ", 120), music.PlaybackMode.InBackground)
+        music.play(music.stringPlayable("E D C D E E E E ", 120), music.PlaybackMode.InBackground)
+        music.play(music.stringPlayable("D D E D C C C C ", 120), music.PlaybackMode.InBackground)
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.R1) == 1) {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.R2) == 1) {
-        music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
     } else if (ps2controller.button_pressed(ps2controller.PS2Button.L1) == 1) {
-        music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-    } else if (false) {
-    	
+        music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+    } else if (ps2controller.button_pressed(ps2controller.PS2Button.Cross) == 1) {
+        左侧循迹传感器 = pins.digitalReadPin(DigitalPin.P1)
+        右侧循迹传感器 = pins.digitalReadPin(DigitalPin.P2)
+        if (左侧循迹传感器 == 1 && 右侧循迹传感器 == 1) {
+            前进()
+        } else if (左侧循迹传感器 == 1 && 右侧循迹传感器 == 0) {
+            左转()
+        } else if (左侧循迹传感器 == 0 && 右侧循迹传感器 == 1) {
+            右转()
+        } else {
+            停止()
+        }
     } else {
         停止()
         makerobo.MotorRun(makerobo.Motors.Centre, 0)
